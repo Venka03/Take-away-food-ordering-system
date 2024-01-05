407924060793b619128dad12327ce3cfd1b92c66
Venka03 <venka03@knu.ua>
2024-01-05 18:04:06 +0100

    Order class updated

--- Takeaway.cpp
+++ Takeaway.cpp
@@ -37,6 +37,7 @@ public:
 	float getPrice(){
 		return price;
 	}
+	virtual bool isTwoForOne() {return false;} // 2-4-1 is not applied to all products except the once with y in corresponding 2-4-1 column
 };
 
 class Appetiser: public Item {
@@ -58,6 +59,8 @@ public:
 		if (twoForOne) str += " (2-4-1)";
 		return str;
 	}
+	bool isShareable(){ return shareable;}
+	bool isTwoForOne() override {return twoForOne;}
 };
 
 class MainCourse: public Item {
@@ -105,9 +108,14 @@ public:
 		this->items = {};
 	}
 	virtual string toString() = 0;
+	Item* getItem(int i){
+		if (i > items.size() || i <= 0)
+			throw "Number of item is out of the range";
+		return items[i-1];
+	}
 };
 
-class Menu:ItemList {
+class Menu: public ItemList {
 private:
     int numberOfAppetiser;
     int numberOfMainCourse;
@@ -175,18 +183,67 @@ public:
 	}
 };
 
-class Order:ItemList {
+class Order: public ItemList {
 private:
 	float total;
+	vector<float> prices2_4_1;
+	int two4one;
 public:
-	float calculateTotal(){}
-	void printReceipt(){}
-	string toString(){}
-
-
+	Order(){
+		total = 0;
+	};
+	void add(Item* choice){
+		items.push_back(choice);
+	}
+	void remove(Item* choice){
+		vector<Item*>::iterator i = find(items.begin(), items.end(), choice);
+		items.erase(i);
+	}
+	void remove(int i){
+		if (i > items.size() || i <= 0) throw "Number of item is out of the range"; 
+		items.erase(items.begin()+i-1);
+	}
+	void calculateTotal(){
+		two4one = 0; // counter of items with 2-4-1 offer
+		total = 0;
+		prices2_4_1.clear();
+		for (Item* item: items){
+			total += item->getPrice();
+			if (item->isTwoForOne()){
+				two4one++;
+				prices2_4_1.push_back(item->getPrice());
+			}
+		}
+		// rearrange array so first floor(two4one/2) elements are the smallest
+		// floor(two4one/2) since we apply for each second 2-4-1 item the discount
+		partial_sort(prices2_4_1.begin(), prices2_4_1.begin() + two4one/2, prices2_4_1.end());
+		for (int i=0; i<two4one/2; i++)
+			total -= prices2_4_1[i];
+	}
+	void printReceipt(){
+		string address = "receipt.txt";
+		ofstream receipt;
+		receipt.open(address);
+		receipt << toString() << endl; 
+		receipt.close();
+	}
+	string toString(){
+		string order = "";
+        for (int i=0; i<items.size(); i++)
+            order += "(" + to_string(i+1) + ") " + items[i]->toString() + "\n";
+		calculateTotal();
+		int saved = 0;
+		for (int i=0; i<two4one/2; i++)
+			saved += prices2_4_1[i];
+		if (saved != 0)
+			order += "2-4-1 discount applied! Savings: £" + to_string(saved) + "\n";
+		order += "Total: £" + to_string(total);
+		return order;
+	}
 };
 
 int main(){
 	Menu menu ("/Users/venka/Desktop/Take-away food ordering system/menu.csv");
 	cout << menu.toString();
+	Item* item = menu.getItem(3);
 }
